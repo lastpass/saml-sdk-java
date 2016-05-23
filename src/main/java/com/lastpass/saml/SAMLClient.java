6ba40473f478805760fc2cbc7f673298108f9841
Jimmy Cohen-Boulakia <jcohenboulakia@docxa.com>
2016-05-23 14:03:29 +0200

    Allows unsigned assertions if WantAssertionSigned="false"

--- src/main/java/com/lastpass/saml/SAMLClient.java
+++ src/main/java/com/lastpass/saml/SAMLClient.java
@@ -203,14 +203,15 @@ public class SAMLClient
 
         for (Assertion assertion: response.getAssertions()) {
 
-            // Assertion must be signed correctly
-            if (!assertion.isSigned())
-                throw new ValidationException(
-                    "Assertion must be signed");
+            if (this.spConfig.getWantAssertionsSigned()) {
+                // Assertion must be signed correctly
+	            if (!assertion.isSigned())
+	                throw new ValidationException(
+	                    "Assertion must be signed");
 
-            sig = assertion.getSignature();
-            sigValidator.validate(sig);
-
+	            sig = assertion.getSignature();
+	            sigValidator.validate(sig);
+            }
             // Assertion must contain an authnstatement
             // with an unexpired session
             if (assertion.getAuthnStatements().isEmpty()) {
